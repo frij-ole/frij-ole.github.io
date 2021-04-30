1e6f69e326fbf1c2c9758a8f36764413dc470904
root <root@DESKTOP-L3CMJ9T.localdomain>
2021-04-30 21:48:22 +0200

    Updates

---
+++ static/js/main.56aec48b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpfrij-ole.github.io"]=this["webpackJsonpfrij-ole.github.io"]||[]).push([[0],{11:function(t,e,n){"use strict";n.r(e);var i=n(1),r=n.n(i),c=n(3),o=n.n(c),s=(n(8),n(9),n(0));var u=function(){return Object(s.jsx)("h1",{children:"Ole Borchardt"})};o.a.render(Object(s.jsx)(r.a.StrictMode,{children:Object(s.jsx)(u,{})}),document.getElementById("root"))},8:function(t,e,n){},9:function(t,e,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.56aec48b.chunk.js.map
